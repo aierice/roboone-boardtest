a8523bf9b8bfa75ae54c920e830ea74fd654f43e
Aierice <pokopenmtok@yahoo.co.jp>
2016-04-07 00:24:50 +0900

    元のデータ

---
+++ shinkanjissou0401/initialsetting.h
@@ -0,0 +1,112 @@
+void NVIC_Configuration(void);
+void GPIO_Configuration(void);
+void USART2_Configuration(void);
+void USART6_Configuration(void);
+uint32_t timingdelay;
+void tdelay(uint32_t nTime);
+
+void NVIC_Configuration(void){
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;// | USART6_IRQn;	//3/3 USART6_IRQnを追加した。不要かは分からない
+//	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;	//2/22 追加した。不要かは分からない
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+}
+
+void GPIO_Configuration(void){
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
+	GPIO_Init(GPIOC, &GPIO_InitStruct);
+}
+
+void USART2_Configuration(void){
+	//USART2のRXピンはPD6。TXピンはPD5
+	RCC_AHB1PeriphClockCmd( RCC_AHB1Periph_GPIOD, ENABLE);
+	RCC_APB1PeriphClockCmd( RCC_APB1Periph_USART2, ENABLE);
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;//GPIO_Mode_IN_FLOATINGでもGPIO_Mode_IPDでもいいかも
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOD,&GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD,&GPIO_InitStructure);
+
+	GPIO_PinAFConfig(GPIOD , GPIO_PinSource6 , GPIO_AF_USART2);
+	GPIO_PinAFConfig(GPIOD , GPIO_PinSource5 , GPIO_AF_USART2);
+
+	USART_InitTypeDef USART_InitStructure;
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART2,&USART_InitStructure);
+
+	USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+
+	USART_Cmd(USART2,ENABLE);
+}
+
+void USART6_Configuration(void){
+	//USART6のRXピンはPC7、TXピンはPC6
+	RCC_AHB1PeriphClockCmd( RCC_AHB1Periph_GPIOC, ENABLE);
+	RCC_APB2PeriphClockCmd( RCC_APB2Periph_USART6 ,ENABLE);
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;//GPIO_Mode_IN_FLOATINGでもGPIO_Mode_IPDでもいいかも
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC,&GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOC,&GPIO_InitStructure);
+
+	GPIO_PinAFConfig(GPIOC , GPIO_PinSource7 , GPIO_AF_USART6);
+	GPIO_PinAFConfig(GPIOC , GPIO_PinSource6 , GPIO_AF_USART6);
+
+	USART_InitTypeDef USART_InitStructure;
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART6,&USART_InitStructure);
+
+	USART_ITConfig(USART6, USART_IT_RXNE, ENABLE);
+
+	USART_Cmd(USART6,ENABLE);
+}
+
+void SysTick_Handler(void){
+	if(timingdelay){
+		timingdelay--;
+	}
+}
+void tdelay(uint32_t nTime){
+	timingdelay = nTime;
+	while(timingdelay);
+}
---
+++ shinkanjissou0401/main.c
@@ -0,0 +1,54 @@
+//LED2 PA10
+
+#include"stm32f4xx.h"
+#include"initialsetting.h"
+int main( void)
+{
+
+	SystemInit();
+	GPIO_Configuration();
+	NVIC_Configuration();
+	USART2_Configuration();
+	USART6_Configuration();
+	GPIO_SetBits(GPIOA,GPIO_Pin_10);
+	if(SysTick_Config(SystemCoreClock / 1000)){
+		while(1);
+	}
+	while(1){
+//		while(GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_0));
+		GPIO_SetBits(GPIOA,GPIO_Pin_10);
+		tdelay(1000);
+		GPIO_ResetBits(GPIOA,GPIO_Pin_10);
+		tdelay(1000);
+	}
+
+}
+/*
+void USART2_IRQHandler( void){
+	if( USART_GetITStatus( USART2,USART_IT_RXNE) != RESET){
+		USART_ClearFlag( USART2,USART_FLAG_RXNE);
+		( uint8_t) USART_ReceiveData( USART2);
+	}
+}
+
+void send_exp_USART6(void){
+	static uint8_t counter = 0;
+	switch(counter%4){
+	case 0:	while( USART_GetFlagStatus( USART6,USART_FLAG_TXE)==RESET);
+			USART_SendData( USART6,0b10000001);
+			break;
+	case 1:	while( USART_GetFlagStatus( USART6,USART_FLAG_TXE)==RESET);
+			USART_SendData( USART6,0b00000001);
+			break;
+	case 2:	while( USART_GetFlagStatus( USART6,USART_FLAG_TXE)==RESET);
+			USART_SendData( USART6,0b10000010);
+			break;
+	case 3:	while( USART_GetFlagStatus( USART6,USART_FLAG_TXE)==RESET);
+			USART_SendData( USART6,0b00000010);
+			break;
+	default:;
+	}
+	counter++;
+	return;
+}
+*/
